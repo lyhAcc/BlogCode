61d0b2abf1f29c293a9f52148dfab3eb058cffee
lyhAcc <137143836+lyhAcc@users.noreply.github.com>
2023-06-27 02:18:53 +0800

    Add files via upload

---
+++ 代码/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.viewtest1.customviewtest_4category.customviewgroup.example2;
+
+import android.os.Bundle;
+
+import com.example.viewtest1.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_customviewgroup2);
+    }
+}
